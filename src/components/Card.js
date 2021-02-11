467b40deb3e016cf5ce172c46022318343343406
Brendon Henning <brendonhenning@gmail.com>
2021-02-11 09:38:49 -0600

    images dynamically load

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+
 const Card = (props) => {
+  const requestImage = require.context("../assets/img", true);
+  
+  let image = requestImage(`./${props.info.pic}.png`).default
 
   return (
-    <div>
-      <button onClick={()=>props.clicked(props.info.idx)}>{props.info.num}</button>
-    </div>
-  )
-}
+    // <div>
+    //   <button 
+    //     onClick={()=>props.clicked(props.info.idx)}>
+    //     <img src={props.info.pic}></img>
+    //     {props.info.team}
+    //   </button>
+    // </div>
+    <img src={image}></img>
+    )
+  }
 
 export default Card
--- src/components/Data.js
+++ src/components/Data.js
@@ -4,157 +4,157 @@ export default function Data() {
     [
       {
         team: 'Colorado Avalanche',
-        pic: '../images/col.png',
+        pic: 'col',
         idx: 0
       },
       {
         team: 'Vegas Golden Knights',
-        pic: '../images/vgk.png',
+        pic: 'vgk',
         idx: 1
       },
       {
         team: 'St Louis Blues',
-        pic: '../images/stl.png',
+        pic: 'stl',
         idx: 2
       },
       {
         team: 'Arizona Coyotes',
-        pic: '../images/ari.png',
+        pic: 'ari',
         idx: 3
       },
       {
         team: 'Anaheim Ducks',
-        pic: '../images/ana.png',
+        pic: 'ana',
         idx: 4
       },
       {
         team: 'Minnesota Wild',
-        pic: '../images/min.png',
+        pic: 'min',
         idx: 5
       },
       {
         team: 'San Jose Sharks',
-        pic: '../images/sjs.png',
+        pic: 'sjs',
         idx: 6
       },
       {
         team: 'LA Kings',
-        pic: '../images/lak.png',
+        pic: 'lak',
         idx: 7
       },
       {
         team: 'Boston Bruins',
-        pic: '../images/bos.png',
+        pic: 'bos',
         idx: 8
       },
       {
         team: 'Philadelphia Flyers',
-        pic: '../images/phi.png',
+        pic: 'phi',
         idx: 9
       },
       {
         team: 'Washington Capitals',
-        pic: '../images/was.png',
+        pic: 'was',
         idx: 10
       },
       {
         team: 'New York Islanders',
-        pic: '../images/nyi.png',
+        pic: 'nyi',
         idx: 11
       },
       {
         team: 'Pittsburg Penguins',
-        pic: '../images/pit.png',
+        pic: 'pit',
         idx: 12
       },
       {
         team: 'New Jersey Devils',
-        pic: '../images/njd.png',
+        pic: 'njd',
         idx: 13
       },
       {
         team: 'Buffalo Sabres',
-        pic: '../images/buf.png',
+        pic: 'buf',
         idx: 14
       },
       {
         team: 'New York Rangers',
-        pic: '../images/nyr.png',
+        pic: 'nyr',
         idx: 15
       },
       {
         team: 'Tamba Bay Lightning',
-        pic: '../images/tbl.png',
+        pic: 'tbl',
         idx: 16
       },
       {
         team: 'Florida Panthers',
-        pic: '../images/flo.png',
+        pic: 'flo',
         idx: 17
       },
       {
         team: 'Chicago Blackhawks',
-        pic: '../images/chi.png',
+        pic: 'chi',
         idx: 18
       },
       {
         team: 'Columbus Blue Jackets',
-        pic: '../images/cbj.png',
+        pic: 'cbj',
         idx: 19
       },
       {
         team: 'Carolina Hurricanes',
-        pic: '../images/car.png',
+        pic: 'car',
         idx: 20
       },
       {
         team: 'Dallas Stars',
-        pic: '../images/dal.png',
+        pic: 'dal',
         idx: 21
       },
       {
         team: 'Nashville Predators',
-        pic: '../images/nas.png',
+        pic: 'nas',
         idx: 22
       },
       {
         team: 'Detroit Red Wings',
-        pic: '../images/det.png',
+        pic: 'det',
         idx: 23
       },
       {
         team: 'Toronto Maple Leafs',
-        pic: '../images/tor.png',
+        pic: 'tor',
         idx: 24
       },
       {
         team: 'Montreal Canadiens',
-        pic: '../images/mon.png',
+        pic: 'mon',
         idx: 25
       },
       {
         team: 'Edmonton Oilers',
-        pic: '../images/edm.png',
+        pic: 'edm',
         idx: 26
       },
       {
         team: 'Winnipeg Jets',
-        pic: '../images/win.png',
+        pic: 'win',
         idx: 27
       },
       {
         team: 'Calgary Flames',
-        pic: '../images/cal.png',
+        pic: 'cal',
         idx: 28
       },
       {
         team: 'Vancouver Canucks',
-        pic: '../images/van.png',
+        pic: 'van',
         idx: 29
       },
       {
         team: 'Ottawa Senators',
-        pic: '../images/ott.png',
+        pic: 'ott',
         idx: 30
       },
     ]
